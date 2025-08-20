db31cfa34a1ba9abdb794b52d5b4672bb842f171
Arish <webdeveloper2@amvionlabs.com>
2025-08-20 14:40:11 +0530

    asd

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,7 @@
 export default function About() {
   return (
     <div className="py-16 text-center">
-      <h1 className="text-4xl sm:text-6xl font-extrabold mb-4 text-gray-900">
-        About Us
-      </h1>
-      <p className="text-xl text-gray-600 max-w-2xl mx-auto">
-        This is the "About Us" page, also using the same layout.
-      </p>
+     
     </div>
   );
 }
